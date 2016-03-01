20e05ac93350bf5825ea232b4ca4ad99cd9017a0
bzavoevanyy <bzavoevanyy@gmail.com>
2016-03-02 00:54:39 +0500

    done

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-var main = document.getElementsByClassName('main-menu__item'),
+var main = document.getElementsByClassName('main-menu__item');
 	for (var i = 0; i < main.length; i++) {
 		main[i].addEventListener('click', function() {
-			var elem = this.lastElementChild,
+			var elem = this.nextElementSibling,
 			    display = elem.style.display;
 			if (display == "" || display == "none") {
-			elem.style.cssText= "display:block;"; 
+				elem.style.cssText= "display:block;";
+				this.style.cssText="background-color : #ff8663";
+				this.lastElementChild.style.cssText="background-image: url('up.png');";
 			} else {
-			elem.style.cssText= "display:none;";
+				elem.style.cssText= "display:none; ";
+				this.style.cssText="background-color : #e7e7e7";
+				this.lastElementChild.style.cssText="background-image: url('down.png');";
 				}
 		});
 	}
